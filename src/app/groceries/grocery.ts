ef2535111305f9ea8bc6781475f05272a6920bf1
Andrew Barker <andrewbarker96@gmail.com>
2024-03-17 12:19:12 -0500

    Changes to be committed: 	modified:   src/app/groceries/grocery.ts

--- src/app/groceries/grocery.ts
+++ src/app/groceries/grocery.ts
@@ -24,19 +24,17 @@ class Total {
 
 // Create an array of grocery items
 let groceries: Grocery[] = [
-    new Grocery('Milk', 3, 2.99),
+    new Grocery('2% Milk', 3, 2.99),
     new Grocery('Bread', 6, 3.99),
-    new Grocery('Eggs', 11, 1.99),
+    new Grocery('White Eggs', 11, 1.99),
     new Grocery('Butter', 2, 4.99),
-    new Grocery('Cheese', 1, 3.99),
+    new Grocery('Colby Jack Shredded Cheese', 1, 3.99),
     new Grocery('Marinara Sauce', 4, 5.99),
 ];
 
 let total: number = 0;
 groceries.forEach(grocery => {
     total += grocery.total;
-
-    return "Total: $" + total.toFixed(2);
 });
 
 export { Grocery, groceries, total };
